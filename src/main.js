2713d3a1b966e93601dad7c83c607675ce032ed3
Edukskj <trindadev494@gmail.com>
2024-03-23 11:35:58 -0300

    finish localStorage

--- src/main.js
+++ src/main.js
@@ -1,154 +1,12 @@
-const meusAlbuns = [
-  {
-    "nome_do_album": "Dos Prédios Deluxe",
-    "nome_da_banda": "Veigh",
-    "ano_do_album": 2023,
-    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b273ce0947b85c30490447dbbd91",
-    "link": "https://open.spotify.com/intl-pt/album/0D59BFMmnA87oPyk1oSe1b?autoplay=true"
-  },
-  {
-    "nome_do_album": "Confia",
-    "nome_da_banda": "Grupo Menos é Mais",
-    "ano_do_album": 2023,
-    "imagem_da_capa_do_album": "https://cdns-images.dzcdn.net/images/cover/dff8386e138eca18ae10bfe307505f68/500x500.jpg",
-    "link": "https://open.spotify.com/intl-pt/track/1jxQZzlx5dcoGhalvryKAA?autoplay=true"
-  },
-  {
-    "nome_do_album": "América Brasil",
-    "nome_da_banda": "Seu Jorge",
-    "ano_do_album": 2007,
-    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b27382b0e4d3c4a8ce89f249731e",
-    "link": "https://open.spotify.com/intl-pt/album/0ooQQDn84CzSsT9c8MFrm1"
-  },
-  {
-    "nome_do_album": "Night Visions",
-    "nome_da_banda": "Imagine Dragons",
-    "ano_do_album": 2012,
-    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b27381eeace38068e9444cb28a3e",
-    "link": "https://open.spotify.com/intl-pt/album/6nxDQi0FeEwccEPJeNySoS"
-  },
-  {
-    "nome_do_album": "O Descobridor dos Sete Mares",
-    "nome_da_banda": "Tim Maia",
-    "ano_do_album": 1983,
-    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b273bf96aed03a1e378dad91217b",
-    "link": "https://open.spotify.com/intl-pt/album/7Ff0Q5oeEoVKHtxJJoXyId"
-  },
-  {
-    "nome_do_album": "The Beginning",
-    "nome_da_banda": "Fifty Fifty",
-    "ano_do_album": 2023,
-    "imagem_da_capa_do_album": "https://www.z90.com/wp-content/uploads/2023/05/Fifty-Cover-Web.jpg",
-    "link": "https://open.spotify.com/intl-pt/album/5letLUZIFsQikJYShfGNs4"
-  },
-  {
-    "nome_do_album": "FULL MOON",
-    "nome_da_banda": "Junko Yagami",
-    "ano_do_album": 1983,
-    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b273bd24ea2859bda2669fe4f176",
-    "link": "https://open.spotify.com/intl-pt/album/4GFqpcjVjJkxYA4xx9Nn0N"
-  },
-  {
-    "nome_do_album": "At Night, Alone",
-    "nome_da_banda": "Mike Posner",
-    "ano_do_album": 2016,
-    "imagem_da_capa_do_album": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMgENBlPAOWN4A_PN17Jx9x8BH3a_YWnL304eUy4QY3aIIVzY9R5qY3bi19Lv7gjcJw8w&usqp=CAU",
-    "link": "https://open.spotify.com/intl-pt/album/6Phl1V5P0sPrWJytXHGFeO"
-  },
-  {
-    "nome_do_album": "Label 67",
-    "nome_da_banda": "Atitude 67",
-    "ano_do_album": 2020,
-    "imagem_da_capa_do_album": "https://akamai.sscdn.co/letras/360x360/albuns/a/2/d/3/745321562335424.jpg",
-    "link": "https://open.spotify.com/intl-pt/album/3EipZjFInVdWgrda8PgoZf"
-  },
-  {
-    "nome_do_album": "MC Leozinho",
-    "nome_da_banda": "Se Ela Dança, Eu Danço",
-    "ano_do_album": 2006,
-    "imagem_da_capa_do_album": "https://akamai.sscdn.co/uploadfile/letras/albuns/0/6/7/8/11553.jpg",
-    "link": "https://music.youtube.com/channel/UCCIXcMUkzdednnKrRTWXsZQ"
-  },
-  {
-    "nome_do_album": "After Hours",
-    "nome_da_banda": "The Weeknd",
-    "ano_do_album": 2020,
-    "imagem_da_capa_do_album": "https://m.media-amazon.com/images/I/813XseYNw1L._UF1000,1000_QL80_.jpg",
-    "link": "https://open.spotify.com/intl-pt/album/4yP0hdKOZPNshxUOjY0cZj"
-  },
-  {
-    "nome_do_album": "Random Access Memories",
-    "nome_da_banda": "Daft Punk",
-    "ano_do_album": 2013,
-    "imagem_da_capa_do_album": "https://i1.sndcdn.com/artworks-000137479333-rpqogd-t500x500.jpg",
-    "link": "https://open.spotify.com/intl-pt/album/4m2880jivSbbyEGAKfITCa"
-  },
-  {
-    "nome_do_album": "The Marshall Mathers LP",
-    "nome_da_banda": "Eminem",
-    "ano_do_album": 2000,
-    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b273dbb3dd82da45b7d7f31b1b42",
-    "link": "https://open.spotify.com/intl-pt/album/6t7956yu5zYf5A829XRiHC"
-  },
-  {
-    "nome_do_album": "Heaven Knows",
-    "nome_da_banda": "PinkPantheress",
-    "ano_do_album": 2023,
-    "imagem_da_capa_do_album": "https://upload.wikimedia.org/wikipedia/en/2/20/PinkPantheress_-_Heaven_Knows.png",
-    "link": "https://open.spotify.com/intl-pt/album/2pOEFqvfxp5uUQ8vQEmVA0"
-  },
-  {
-    "nome_do_album": "Em Sua Direção",
-    "nome_da_banda": "Péricles",
-    "ano_do_album": 2018,
-    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b2732de4d72ae0fab9e563ee4acf",
-    "link": "https://open.spotify.com/intl-pt/album/2ki501yEODlxPWKSRMnMDl"
-  },
-  {
-    "nome_do_album": "Alma Brasileira (Ao Vivo)",
-    "nome_da_banda": "Diogo Nogueira",
-    "ano_do_album": 2016,
-    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b273a3867a19c1d2fae11a1424e6",
-    "link": "https://open.spotify.com/intl-pt/album/5yUuIL8h5dfNO0OBuvZHLE"
-  },
-  {
-    "nome_do_album": "Tim Maia e Convidados",
-    "nome_da_banda": "Tim Maia",
-    "ano_do_album": 1977,
-    "imagem_da_capa_do_album": "https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Coldplay_X%26Y.svg/1920px-Coldplay_X%26Y.svg.png",
-    "link": "https://open.spotify.com/intl-pt/album/59NOU4R72nphHRtwQlGfjJ"
-  },
-  {
-    "nome_do_album": "Toquinho",
-    "nome_da_banda": "Toquinho",
-    "ano_do_album": 1970,
-    "imagem_da_capa_do_album": "https://akamai.sscdn.co/letras/360x360/albuns/4/7/f/b/869201585152394.jpg",
-    "link": "https://music.youtube.com/playlist?list=OLAK5uy_kGoCgadUfljX0SGkqP6ivqlEBS0obQ1mA"
-  },
-  {
-    "nome_do_album": "Máquina do Tempo",
-    "nome_da_banda": "Matuê",
-    "ano_do_album": 2020,
-    "imagem_da_capa_do_album": "https://s2-g1.glbimg.com/4YCTQOs7a47qPX73Lm25vw3rows=/0x0:1600x1600/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/W/A/7wBV8eTXeqgitO5JASRg/capa-matue-mdt.jpg",
-    "link": "https://open.spotify.com/intl-pt/album/6ehm0SMBBoSxH8oSrFXre6"
-  },
-  {
-    "nome_do_album": "Love Island: Pool Party 2019",
-    "nome_da_banda": "Amerie",
-    "ano_do_album": 2019,
-    "imagem_da_capa_do_album": "https://upload.wikimedia.org/wikipedia/en/8/88/Amerie_-_1_Thing.png",
-    "link": "https://www.google.com/search?sca_esv=59a29399512378e1&rlz=1C1GCEU_pt-BRBR1095BR1095&q=1+thing+love+island:+pool+party+2019+-+ministry+of+sound&si=AKbGX_rO4P19IF_yO85wYpkEaz-W_oZWd5JUOOVnUVftf2aeoXP4sfYmHWs581PHBda9zuniR66pzmLzMUncSGXvqUY2a5WHh8E4HJpULRUWh4kPxkoImBnmADmJdTN8Y6fPDyUvCkp0Usm6ZNqGGqNhBCHiGfEvJqKbFQt37A9DtQptNGkbamz_aysApZJin8Idn05W2rRSQD9q3SvEGfZGMw5R3FlHotMVnTxKaTOFMlmWrVL6RsmTNynsWuOJyHWf1jHNvO0wh3bsi6rt7vVw_7v_XbqqNg%3D%3D&sa=X&ved=2ahUKEwili4_Ut4-EAxWJBrkGHe-lBV0QmxMoAHoECDYQAg&biw=1440&bih=773&dpr=1"
-  }
-];
-
 // Add LocalStorage
 
 let albumStorage = "albuns";
-
-localStorage.setItem(albumStorage,JSON.stringify(meusAlbuns));
 let data = localStorage.getItem(albumStorage);
+let userData = [];
 
-const userData = JSON.parse(data);
+if (JSON.parse(data) != null) {
+  userData = JSON.parse(data);
+}
 
 // Functions
 
@@ -210,8 +68,7 @@ function criaListaDeAlbuns(linhas) {
 
 function atualizaListaDeAlbuns() {
   const listaDeAlbuns = document.getElementById("album-list")
-
-  listaDeAlbuns.replaceWith(criaListaDeAlbuns(organizaEmLinhasEColunas(meusAlbuns)))
+  listaDeAlbuns.replaceWith(criaListaDeAlbuns(organizaEmLinhasEColunas(userData)))
 };
 
 atualizaListaDeAlbuns();
@@ -219,7 +76,7 @@ atualizaListaDeAlbuns();
 function randomizacao() {
   const listaDeAlbuns = document.getElementById("album-list")
 
-  listaDeAlbuns.replaceWith(criaListaDeAlbuns(organizaEmLinhasEColunas(meusAlbuns.sort(() => Math.random() - 0.5))))
+  listaDeAlbuns.replaceWith(criaListaDeAlbuns(organizaEmLinhasEColunas(userData.sort(() => Math.random() - 0.5))))
 };
 
 // Search Input
@@ -277,13 +134,15 @@ function adicionar() {
     let link = document.getElementById("link");
 
     if (nome_do_album.value !== "" && nome_da_banda.value !== "" && ano_do_album.value !== "" && imagem_da_capa_do_album !== "" && link.value !== "") {
-      meusAlbuns.push({
+
+      userData.push({
         "nome_do_album": `${nome_do_album.value}`,
         "nome_da_banda": `${nome_da_banda.value}`,
         "ano_do_album": `${ano_do_album.value}`,
         "imagem_da_capa_do_album": `${imagem_da_capa_do_album.value}`,
         "link": `${link.value}`
       });
+      localStorage.setItem(albumStorage,JSON.stringify(userData));
 
       document.getElementById("albuns").style.display = "";
       document.getElementById("form").style.display = "none";
